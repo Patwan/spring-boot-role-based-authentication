ee134dac5bf1369eb29b8f3fa4b4c3b47df7bc56
Martin Njogu <mndungu730@gmail.com>
2020-12-15 17:33:27 +0300

    Created Join User endpoint

--- src/main/java/com/pwebk/SpringBootrolebasedauth/config/SecuityConfig.java
+++ src/main/java/com/pwebk/SpringBootrolebasedauth/config/SecuityConfig.java
@@ -1,12 +1,14 @@
 package com.pwebk.SpringBootrolebasedauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +21,10 @@ public class SecuityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/pwebk/SpringBootrolebasedauth/controller/UserController.java
+++ src/main/java/com/pwebk/SpringBootrolebasedauth/controller/UserController.java
@@ -1,10 +1,34 @@
 package com.pwebk.SpringBootrolebasedauth.controller;
 
+import com.pwebk.SpringBootrolebasedauth.entity.User;
+import com.pwebk.SpringBootrolebasedauth.repository.UserRepository;
+import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final String DEFAULT_USER = "ROLE_USER";
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @PostMapping("/join")
+    public String joinGroup(@RequestBody User user){
+        user.setRoles(DEFAULT_USER);
+        String encryptedPwd = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPwd);
+        repository.save(user);
+        return "Hi" + user.getUserName() + " ,welcome to group";
+    }
 
 }
