fb4073b0517fe4dc01486c099870194f78e6129b
Martin Njogu <mndungu730@gmail.com>
2020-12-16 07:25:04 +0300

    Added Spring Security Routes Middleware

--- src/main/java/com/pwebk/SpringBootrolebasedauth/config/SecuityConfig.java
+++ src/main/java/com/pwebk/SpringBootrolebasedauth/config/SecuityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,6 +22,15 @@ public class SecuityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().
+                antMatchers("/user/join").permitAll()
+                .and().authorizeRequests().
+                antMatchers("/user/**").authenticated().and().httpBasic();
     }
 
     @Bean
--- src/main/java/com/pwebk/SpringBootrolebasedauth/controller/UserController.java
+++ src/main/java/com/pwebk/SpringBootrolebasedauth/controller/UserController.java
@@ -2,19 +2,24 @@ package com.pwebk.SpringBootrolebasedauth.controller;
 
 import com.pwebk.SpringBootrolebasedauth.entity.User;
 import com.pwebk.SpringBootrolebasedauth.repository.UserRepository;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Id;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     private static final String DEFAULT_USER = "ROLE_USER";
+    private static final String[] ADMIN_ACCESS = {"ROLE_ADMIN" , "ROLE_MODERATOR"};
+    private static final String[] MODERATOR_ACCESS = {"ROLE_MODERATOR"};
 
     @Autowired
     private UserRepository repository;
@@ -29,6 +34,38 @@ public class UserController {
         user.setPassword(encryptedPwd);
         repository.save(user);
         return "Hi" + user.getUserName() + " ,welcome to group";
+    }
+
+    //If Logged in user is ADMIN -> ADMIN or MODERATOR
+    //If loggedin is Moderator -> MODERATOR
+    @GetMapping("/access/{userId}/{userRole}")
+    public String giveAccessToUser(@PathVariable Id userId, @PathVariable String userRole, Principal principal){
+        User user = repository.findById(userId).get();
+        List<String> activeRoles = getRolesByLoggedInUser(principal);
+
+        String newRole ="";
+        if(activeRoles.contains(userRole)){
+            newRole = user.getRoles() + "," + userRole;
+            user.setRoles(newRole);
+        }
+        repository.save(user);
+        return "Hi " + user.getUserName() + " New role assigned to you by " + principal.getName();
+    }
+
+    private List<String> getRolesByLoggedInUser(Principal principal){
+        String roles = getLoggedInUser(principal).getRoles();
+        List<String> assignRoles = Arrays.stream(roles.split(",")).collect(Collectors.toList());
+        if(assignRoles.contains("ROLE_ADMIN")){
+            return Arrays.stream(ADMIN_ACCESS).collect((Collectors.toList()));
+        }
+        if(assignRoles.contains("ROLE_MODERATOR")){
+            return Arrays.stream(MODERATOR_ACCESS).collect((Collectors.toList()));
+        }
+        return Collections.emptyList();
+    }
+
+    private User getLoggedInUser(Principal principal){
+        return repository.findByUserName(principal.getName()).get();
     }
 
 }
